f4955e0fb164020d586d4da2f7ffbf17a051951c
GeorgiaLR <cordonnier.l@gmail.com>
2020-06-20 12:31:04 +0200

    Adding TP Chat Node.js avec socket.io

--- OC-Nodejs/app.js
+++ OC-Nodejs/app.js
@@ -1,21 +1,43 @@
-// Appelle le module http
-var http = require('http');
 // Appelle le module url
 var url = require('url');
 // Module qui parse la chaine des paramètres de l'url
 var queryString = require('querystring');
 // Module de routing
 var express = require('express');
-var bodyParser = require('body-parser')
-
-var todoCrud = require('./Todolist/todo')
+var bodyParser = require('body-parser');
+var todoCrud = require('./Todolist/todo');
 
 var app = express();
+// Appelle le module http
+var http = require('http').createServer(app);
+  
+// Chargement de socket.io
+var io = require('socket.io')(http);
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+var cookieParser = require("cookie-parser");
+
+var session = require("express-session")({
+    secret: "nodeAppSessionKey",
+    resave: true,
+    saveUninitialized: true
+});
+var sharedsession = require("express-socket.io-session");
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+extended: true
 })); 
+//app.use(cookieParser);
+app.use(session);
+
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+  });
+
+app.get('/chat', (req, res) => {
+    res.render('chat.ejs');
+  });
 
 app.get('/todolist', function(req, res) {
     var tasks = todoCrud.getAllTasks();
@@ -61,4 +83,48 @@ app.delete('/todolist/delete/:taskid', function(req, res){
 });
 
 
-app.listen(8080);
+// Use shared session middleware for socket.io
+// setting autoSave:true
+io.use(sharedsession(session, {
+    autoSave:true
+})); 
+
+
+
+
+// Quand un client se connecte, on le note dans la console
+io.on('connection', function (socket) {
+
+    console.log("Guest connected");
+
+    socket.on('connection', () => {
+        io.emit('chat message', "Welcome guest !");
+    });
+    
+    //socket.broadcast.emit("message", "Welcome guest !");
+        
+    // Accept a login event with user's data
+    socket.on("login", function(userdata) {
+        console.log('user connected');
+        socket.handshake.session.userdata = userdata;
+        socket.handshake.session.save();
+    });
+
+    socket.on("logout", function(userdata) {
+        console.log('user disconnected');
+        if (socket.handshake.session.userdata) {
+            delete socket.handshake.session.userdata;
+            socket.handshake.session.save();
+        }
+    });        
+
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+});
+
+
+
+http.listen(8080);
